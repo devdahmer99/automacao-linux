616798ac27e50a809128fd1ebb1080f778b61e22
Eduardo Dahmer <80227515+devdahmer99@users.noreply.github.com>
2022-03-09 23:05:18 -0300

    Atualização de funcionalidades
    adição de novos softwares para instalação

--- automatiza.sh
+++ automatiza.sh
@@ -3,14 +3,14 @@
 # Caso exista alguma trava de um processo.
 sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock;
 
-# Instalação do Softwares-Properties-Common e Adição do PPA do PHP7.4
+# Instalação do Softwares-Properties-Common e Adição do PPA do PHP
 sudo apt install software-properties-common ; sudo add-apt-repository ppa:ondrej/php ;
 
 # Atualizaçao geral de repositórios
 sudo apt update &&
 
 # Instalacao de pacotes e programas nativos do repositorio principal do Ubuntu
-sudo apt install python3 git curl libssl-dev php8.0 composer php8.0-common php8.0-mysql php8.0-xml php8.0-xmlrpc php8.0-curl php8.0-gd php8.0-imagick php8.0-cli php8.0-dev php8.0-imap php8.0-mbstring php8.0-opcache php8.0-soap php8.0-zip php8.0-intl php8.0-xdebug php8.0-pgsql -y &&
+sudo apt install python3 git curl libssl-dev php8.1 composer php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-xdebug php8.1-pgsql -y &&
 
 
 # Instalaçao dos softwares que utilizam o gerenciador de pacotes snap
@@ -20,9 +20,18 @@ sudo snap install phpstorm --classic &&
 sudo snap install postman &&
 sudo snap install node --classic &&
 sudo snap install code --classic &&
-sudo snap install beekeeper-studio &&
+sudo snap install dbeaver-ce &&
+sudo snap install intellij-idea-community --classic &&
+sudo snap install clion --classic && 
+sudo snap install cmake --classic &&
+sudo snap install spotify &&
+sudo snap install vlc && 
+sudo snap install ramboxpro &&
 
 # Softwares que precisam de Download externo
+
+# Caso ainda exista alguma trava de um processo.
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock;
 
 cd ~/Downloads/ &&wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb 
 
