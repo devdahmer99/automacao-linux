8c4315051bd0578c6a6242917e53a78e60d55f5b
Eduardo Dahmer <80227515+devdahmer99@users.noreply.github.com>
2022-03-09 23:23:57 -0300

    Update automatiza.sh

--- automatiza.sh
+++ automatiza.sh
@@ -6,12 +6,20 @@ sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock;
 # Instalação do Softwares-Properties-Common e Adição do PPA do PHP
 sudo apt install software-properties-common ; sudo add-apt-repository ppa:ondrej/php ;
 
+#Instalação do Docker e Docker-Compose
+sudo apt install docker && apt install docker-compose -y
+
 # Atualizaçao geral de repositórios
 sudo apt update &&
 
 # Instalacao de pacotes e programas nativos do repositorio principal do Ubuntu
 sudo apt install python3 git curl libssl-dev php8.1 composer php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl php8.1-xdebug php8.1-pgsql -y &&
 
+#Instalação PostgreSQL e PGAdmin 4
+sudo apt install postgresql-12 -y
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -y
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4
 
 # Instalaçao dos softwares que utilizam o gerenciador de pacotes snap
 
